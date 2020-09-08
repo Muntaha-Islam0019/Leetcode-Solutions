fa9c9819ffd027f8e0a17989d6e91bb06ac61ea3
Muntaha Islam <muntaha.islam0019@gmail.com>
2020-09-09 03:42:23 +0600

    Kids_With_the_Greatest_Number_of_Candies

---
+++ Kids_With_the_Greatest_Number_of_Candies.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        List<Boolean> finalList = new ArrayList<Boolean>();
+
+        int highest = candies[0];
+        for (int i = 1; i < candies.length; i++) 
+            if (candies[i] > highest) 
+                highest = candies[i];
+
+        for (int j = 0; j < candies.length; j++) 
+            candies[j] = candies[j] + extraCandies;
+
+        for (int k = 0; k < candies.length; k++) 
+            if (candies[k] >= highest)
+                finalList.add(true);
+            else
+                finalList.add(false);
+        
+        return finalList;
+    }
+}
