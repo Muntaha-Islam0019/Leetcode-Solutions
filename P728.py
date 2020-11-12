db6cedc5139cb7ce0df4f519cb521e15e9851c2e
Muntaha Islam <muntaha.islam0019@gmail.com>
2020-11-13 01:01:20 +0600

    Problem 728

---
+++ P728.py
@@ -0,0 +1,5 @@
+class Solution:
+    def selfDividingNumbers(self, left, right):
+
+        return [num for num in range(left, right+1)
+                if '0' not in str(num) and all(num % int(c) == 0 for c in str(num))]
