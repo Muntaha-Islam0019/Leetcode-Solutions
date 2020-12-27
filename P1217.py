c0abe558bed4e1b1c9ebdf0130e34c8a9a3617ad
Muntaha Islam <muntaha.islam0019@gmail.com>
2020-12-27 20:14:36 +0600

    Problem 1217

---
+++ P1217.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minCostToMoveChips(self, chips: List[int]) -> int:
+        O=0
+        E=0
+        for x in chips:
+            if x % 2 == 0:
+                E += 1
+            else:
+                O += 1
+        return min(E, O)
