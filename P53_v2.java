7f91265a1896d3e86e171ef93e3b470de0ece451
Md. Muntaha Islam <muntaha.islam0019@gmail.com>
2025-08-22 19:57:12 +0600

    Problem 53 with Kadane's Algo

---
+++ P53_v2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            // either take this element alone or extend the subarray
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
